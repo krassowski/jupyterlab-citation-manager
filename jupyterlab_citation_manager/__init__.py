f980bd6fc3960e4cd02c10a3223c4e680a92f55f
krassowski <5832902+krassowski@users.noreply.github.com>
2023-12-13 13:38:55 +0000

    Update `__init__.py`

--- jupyterlab_citation_manager/__init__.py
+++ jupyterlab_citation_manager/__init__.py
@@ -1,20 +1,19 @@
-
-import json
-from pathlib import Path
-
+try:
+    from ._version import __version__
+except ImportError:
+    # Fallback when using the package in dev mode without installing
+    # in editable mode with pip. It is highly recommended to install
+    # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
+    import warnings
+    warnings.warn("Importing 'jupyterlab_citation_manager' outside a proper installation.")
+    __version__ = "dev"
 from .handlers import setup_handlers
-from ._version import __version__
-
-HERE = Path(__file__).parent.resolve()
-
-with (HERE / "labextension" / "package.json").open() as fid:
-    data = json.load(fid)
 
 
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": data["name"]
+        "dest": "jupyterlab-citation-manager"
     }]
 
 
@@ -36,6 +35,3 @@ def _load_jupyter_server_extension(server_app):
         f"Registered jupyterlab_citation_manager extension at URL path /{url_path}"
     )
 
-
-# For backward compatibility with the classical notebook
-load_jupyter_server_extension = _load_jupyter_server_extension
